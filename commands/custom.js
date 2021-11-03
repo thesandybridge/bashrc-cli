c2b878d1ee4f4f7faaedff63745c2c17f715364f
Matt Miller <mmiller@grafik.com>
2021-11-02 23:18:18 -0400

    started to create new command
    Co-authored-by: Kurt M <kurtmilla1994@users.noreply.github.com>

---
+++ commands/custom.js
@@ -0,0 +1,20 @@
+const conf = new (require('conf'))()
+const fs = require('fs')
+const path = require('path')
+const homedir = require('os').homedir();
+
+function createCustomFunction (name) {
+    const output = `\n alias ${alias}="${command}"`;
+    const file = path.join(homedir,'/', '.bash_aliases')
+    
+    fs.appendFile(file, output, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          // Get the file contents after the append operation
+          console.log(`${output} added to .bash_aliases`);
+        }
+    });
+}
+module.exports = createCustomFunction
--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 const { program } = require('commander')
 
 const alias = require('./commands/alias')
+const custom = require('./commands/custom')
 
 program
         .command('create <alias> <command>')
         .description('Create a bash alias')
         .action(alias)
+
+program
+        .command('fn <name>')
+        .description('Create a custom bash function with flags')
+        .action(custom)
 
 program.parse()
